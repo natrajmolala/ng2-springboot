ca62d863a7ef77bad2db91ff53e1c30a80c51e6b
Natraj Molala <natraj.molala@agilesphere.co.uk>
2017-02-20 22:21:56 +0000

    resolve browser refresh issue

--- server/src/main/java/com/springboot/server/config/WebMvcConfiguration.java
+++ server/src/main/java/com/springboot/server/config/WebMvcConfiguration.java
@@ -1,5 +1,7 @@
 package com.springboot.server.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
@@ -10,22 +12,26 @@ import java.io.File;
 @Configuration
 public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(WebMvcConfiguration.class.getCanonicalName());
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         // TODO: add this to dev profile
         String currentPath = new File(".").getAbsolutePath();
         currentPath = "file:///" + currentPath;
 
+        LOGGER.info("currentPath: " + currentPath);
+
         // This one adds a link to the distribution folder
         registry.addResourceHandler("/**")
-                .addResourceLocations(currentPath + "/../client/dist/");
+                .addResourceLocations(currentPath + "/client/dist/");
 
         // This one adds a link to the sources folder for debugging in the browser. This must be configured in conjuction
         // with the following configuration in tsconfig.json file:
         // "sourceMap": true  -> generate source maps so that we can link js with ts files for debugging
         // "sourceRoot": "/app-src" -> same as configured below (this way we get a clean separation between js files and ts files on the client)
         registry.addResourceHandler("/app-src/**")
-                .addResourceLocations(currentPath + "/../client/src/");
+                .addResourceLocations(currentPath + "/client/src/");
     }
 
     @Override
--- server/src/main/java/com/springboot/server/controller/RootController.java
+++ server/src/main/java/com/springboot/server/controller/RootController.java
@@ -1,13 +1,18 @@
 package com.springboot.server.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class RootController {
 
-    @RequestMapping(value = {"/home", "/about", "/owner", "/vet/**"})
+    Logger logger = LoggerFactory.getLogger(RootController.class.getCanonicalName());
+
+    @RequestMapping(value = {"/", "/home", "/about", "/owner", "/vet/**"})
     public String auth() {
+        logger.info("Inside auth.....");
         return "/index.html";
     }
 
